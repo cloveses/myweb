5f9cf42e006b6f6665284917470511dc26f49982
djx <1181564186@qq.com>
2017-03-16 08:46:45 +0800

    edit

--- tools/verify_gen.py
+++ tools/verify_gen.py
@@ -3,6 +3,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import io
 import random
+import os
 
 def get_text():
     text = []
@@ -28,9 +29,10 @@ def make_noises(drw,lines=3):
     drw.point(pnt,fill=get_color())
 
 def get_verify():
+    path = os.path.split(os.path.realpath(__file__))[0]
     a = Image.new('RGB',(80,50),'white')
     drw = ImageDraw.Draw(a)
-    myfont = ImageFont.truetype('swiss.ttf',size=20)
+    myfont = ImageFont.truetype(os.path.join(path,'swiss.ttf'),size=20)
     atext = get_text()
     drw.text((10,10),atext,font=myfont,fill='green')
     make_noises(drw)
